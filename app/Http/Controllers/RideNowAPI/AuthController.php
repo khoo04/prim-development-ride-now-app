6f45579cbde362d63231ded24b09f3d0d5aa1235
Khoo Zhen Xian <khoozhenxian@gmail.com>
2024-12-28 20:35:51 +0800

    Updated: Auth Message

--- app/Http/Controllers/RideNowAPI/AuthController.php
+++ app/Http/Controllers/RideNowAPI/AuthController.php
@@ -77,7 +77,7 @@ class AuthController extends Controller
 
         return response()->json([
             "success" => false,
-            'message' => 'Unauthorized'
+            'message' => 'Invalid credentials'
         ], 401);
     }
 
